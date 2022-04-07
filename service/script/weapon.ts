aaffe2acb6c65f1dd505c7811d8ef85f5cd0db69
Punn Nararatwong <punn.nara@gmail.com>
2022-04-07 22:30:56 +0700

    Add getting weapon url script

--- service/script/weapon.ts
+++ service/script/weapon.ts
@@ -2,43 +2,43 @@ import { Browser } from 'playwright';
 
 const WEAPON_TYPE_URL = [
     'https://genshin-impact.fandom.com/wiki/Bows',
-    // 'https://genshin-impact.fandom.com/wiki/Catalysts',
-    // 'https://genshin-impact.fandom.com/wiki/Claymores',
-    // 'https://genshin-impact.fandom.com/wiki/Polearms',
-    // 'https://genshin-impact.fandom.com/wiki/Swords',
+    'https://genshin-impact.fandom.com/wiki/Catalysts',
+    'https://genshin-impact.fandom.com/wiki/Claymores',
+    'https://genshin-impact.fandom.com/wiki/Polearms',
+    'https://genshin-impact.fandom.com/wiki/Swords',
 ];
 
+async function getUrlList(browser: Browser, url: string): Promise<string[]> {
+    const page = await browser.newPage();
+    await page.goto(url, { timeout: 0 });
+
+    return page.$eval('body', (element) => {
+        const list: string[] = [];
+        const row = element.querySelectorAll<HTMLTableElement>('.article-table')[1].tBodies[0].children;
+
+        for (let index = 0; index < row.length; index += 1) {
+            const cell = row[index];
+            const dataUrl = cell.querySelector<HTMLAnchorElement>('td > a')?.href;
+
+            if (!dataUrl) continue;
+
+            list.push(dataUrl);
+        }
+
+        return list;
+    });
+}
+
 async function exportWeapon(browser: Browser) {
-    // const urlList: string[] = [];
-    // const promiseArr: Promise<string[]>[] = [];
-    let data: string[] = [];
+    const promiseArr: Promise<string[]>[] = [];
 
     WEAPON_TYPE_URL.forEach(async (url) => {
-        const page = await browser.newPage();
-        await page.goto(url, { timeout: 0 });
-
-        // promiseArr.push(
-        data = await page.$eval('body', (element) => {
-            const list: string[] = [];
-            const row = element.querySelectorAll<HTMLTableElement>('.article-table')[1].tBodies[0].children;
-
-            for (let index = 0; index < row.length; index += 1) {
-                const cell = row[index];
-                const dataUrl = cell.querySelector<HTMLAnchorElement>('td > a')?.href;
-
-                if (!dataUrl) continue;
-
-                list.push(dataUrl);
-            }
-
-            return list;
-        });
-
-        console.log(`result :${data}`);
-        // );
+        promiseArr.push(getUrlList(browser, url));
     });
 
-    // const result = await Promise.all(promiseArr);
+    const result = await Promise.all(promiseArr);
+
+    console.log(result);
 }
 
 export default exportWeapon;
