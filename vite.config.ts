22fc8b6e4f0cbe45fb304ded9b51b497f780b784
Punn Nararatwong <punn.nara@gmail.com>
2022-04-16 20:37:13 +0700

    Change vite build to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   resolve: {
     alias: [{ find: '@data', replacement: path.resolve(__dirname, './data') }],
   },
