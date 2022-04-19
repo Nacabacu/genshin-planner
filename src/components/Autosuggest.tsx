b52f26c289cc7a56d91c97e75d3648cb5c4433cd
Punn Nararatwong <punn.nara@gmail.com>
2022-04-19 21:49:49 +0700

    Add not found item in Autosuggest

--- src/components/Autosuggest.tsx
+++ src/components/Autosuggest.tsx
@@ -76,6 +76,67 @@ function Autosuggest<T, Multiple extends boolean | undefined = undefined>({
     selectedElement?.scrollIntoView();
   }, [isMenuOpened, value, getLabel]);
 
+  const renderItems = () => {
+    const filteredItems = items.filter(
+      (item) => hasSelected.current || getLabel(item).toLowerCase().includes(filterString.toLocaleLowerCase()),
+    );
+
+    if (filteredItems.length > 0) {
+      return filteredItems.map((item) => {
+        const label = getLabel(item);
+        const isSelected = Array.isArray(value) ? value.find((i) => getLabel(i) === label) : getLabel(value) === label;
+
+        return (
+          <button
+            key={label}
+            type="button"
+            data-name={label}
+            className={`active:bg-gray-500" px-4 py-2 hover:bg-gray-600 ${
+              isSelected ? 'bg-cyan-600 hover:bg-cyan-500' : ''
+            }`}
+            onClick={() => {
+              let newValue: ValueType<T, Multiple>;
+
+              if (Array.isArray(value)) {
+                if (value.includes(item)) closeMenu();
+                newValue = [...value, item] as ValueType<T, Multiple>;
+              } else if (multiple) {
+                newValue = [item] as ValueType<T, Multiple>;
+              } else {
+                if (item === value) closeMenu();
+                newValue = item as ValueType<T, Multiple>;
+              }
+
+              onSelect(newValue);
+
+              if (resetAfterSelect) {
+                setValue(null);
+                setFilterString('');
+                closeMenu();
+              } else {
+                setValue(newValue);
+                setFilterString(multiple ? '' : getLabel(item));
+                closeMenu(newValue);
+              }
+            }}
+          >
+            <div className="flex items-center">
+              {getStartAdornment && <span className="mr-4 flex h-6 w-6 items-center">{getStartAdornment(item)}</span>}
+              <span className="overflow-hidden whitespace-nowrap">{getLabel(item)}</span>
+            </div>
+          </button>
+        );
+      });
+    }
+    return (
+      <button type="button" className="cursor-default px-4 py-2 text-gray-500">
+        <div className="flex items-center">
+          <span className="overflow-hidden whitespace-nowrap">{resources.empty_autosuggest_label}</span>
+        </div>
+      </button>
+    );
+  };
+
   return (
     <div className={`relative ${className}`} ref={wrapperRef}>
       <div
@@ -105,7 +166,7 @@ function Autosuggest<T, Multiple extends boolean | undefined = undefined>({
             }
             openMenu();
           }}
-          placeholder={placeholder || resources.default_autosuggest_label}
+          placeholder={placeholder || resources.default_autosuggest_placeholder}
           ref={inputRef}
         />
         <ArrowDropDown
@@ -126,59 +187,7 @@ function Autosuggest<T, Multiple extends boolean | undefined = undefined>({
           className="absolute z-10 mt-1 flex max-h-60 w-full flex-col overflow-scroll rounded bg-gray-700 py-1 text-gray-300 shadow-xl"
           ref={popupRef}
         >
-          {items
-            .filter(
-              (item) => hasSelected.current || getLabel(item).toLowerCase().includes(filterString.toLocaleLowerCase()),
-            )
-            .map((item) => {
-              const label = getLabel(item);
-              const isSelected = Array.isArray(value)
-                ? value.find((i) => getLabel(i) === label)
-                : getLabel(value) === label;
-
-              return (
-                <button
-                  key={label}
-                  type="button"
-                  data-name={label}
-                  className={`active:bg-gray-500" px-4 py-2 hover:bg-gray-600 ${
-                    isSelected ? 'bg-cyan-600 hover:bg-cyan-500' : ''
-                  }`}
-                  onClick={() => {
-                    let newValue: ValueType<T, Multiple>;
-
-                    if (Array.isArray(value)) {
-                      if (value.includes(item)) closeMenu();
-                      newValue = [...value, item] as ValueType<T, Multiple>;
-                    } else if (multiple) {
-                      newValue = [item] as ValueType<T, Multiple>;
-                    } else {
-                      if (item === value) closeMenu();
-                      newValue = item as ValueType<T, Multiple>;
-                    }
-
-                    onSelect(newValue);
-
-                    if (resetAfterSelect) {
-                      setValue(null);
-                      setFilterString('');
-                      closeMenu();
-                    } else {
-                      setValue(newValue);
-                      setFilterString(multiple ? '' : getLabel(item));
-                      closeMenu(newValue);
-                    }
-                  }}
-                >
-                  <div className="flex items-center">
-                    {getStartAdornment && (
-                      <span className="mr-4 flex h-6 w-6 items-center">{getStartAdornment(item)}</span>
-                    )}
-                    <span className="overflow-hidden whitespace-nowrap">{getLabel(item)}</span>
-                  </div>
-                </button>
-              );
-            })}
+          {renderItems()}
         </div>
       )}
     </div>
