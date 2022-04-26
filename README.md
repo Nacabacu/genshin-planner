113879bbee1a406df3606ef950bef10ca6c3b8fb
Punn Nararatwong <punn.nara@gmail.com>
2022-04-26 21:27:03 +0700

    Add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# [Minimal Genshin Planner](https://genshin-planner.punn.dev)
+
+Minimalist Genshin Impact planner. That's it.
+
+# Development
+
+```
+npm install
+npm run dev
+```
+
+# License
+
+[MIT](https://github.com/Nacabacu/genshin-planner/blob/main/LICENSE)
+
+This project is not affiliated with HoYoVerse.
+Genshin Impact, all materials are trademarks and copyrights of HoYoVerse.
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 import { Locale, useLocalizationContext } from '../contexts/localizationContext';
 import Dropdown from './Dropdown';
 
+// TODO: Fix bug flag is not shown in iOS
 const flagItems: Locale[] = [Locale.English, Locale.Japanese, Locale.Thai];
 const flagMap: Partial<Record<Locale, ReactNode>> = {
   [Locale.English]: <Flags.US />,
@@ -15,6 +16,10 @@ function Footer() {
 
   return (
     <div className="flex h-16 max-w-9xl flex-grow items-center">
+      <div className="mb-2 flex h-full items-end overflow-hidden text-xs">
+        This project is not affiliated with HoYoVerse. <br /> Genshin Impact, all materials are trademarks and
+        copyrights of HoYoVerse.
+      </div>
       <Dropdown
         items={flagItems}
         hideLabel
